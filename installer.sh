09c4f64fb736b9d55ed2818f146b6d19054541a8
cptcr <toowake@proton.me>
2025-09-11 16:47:46 +0200

    fix: Change installer version

--- installer.sh
+++ installer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #############################################
-# VaultScope Statistics Installer v6.2.9
+# VaultScope Statistics Installer v6.3.0
 #############################################
 
 set -e
 
-VSS_VERSION="6.2.9"
+VSS_VERSION="6.3.0"
 INSTALL_DIR_SERVER="/var/www/vs-statistics-server"
 INSTALL_DIR_CLIENT="/var/www/vs-statistics-client"
 INSTALL_DIR_FULL="/var/www/statistics"
